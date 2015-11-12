5cf5abacb1a6d976aaaa7853d114604fb1b7c972
MikailBag <bagishov444.2002.02@gmail.com>
2015-11-12 17:34:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+var ctx=require('./ctx');
+//example of ctx: {read:func,open:func...}
 module.exports=function(generator){
     var gen=generator.call(ctx);
     var result;
     while(true){
     var task=generator.next(result);
+    task(nextIteration)
 //...
-
+//end????
     }
 
 }
