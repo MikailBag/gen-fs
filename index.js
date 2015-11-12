56d153ec9f06381a47dcfdb39912cc07abd06a68
MikailBag <bagishov444.2002.02@gmail.com>
2015-11-12 17:28:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports=function(generator){
-var gen=generator.call(ctx);
-var result;
-while(true){
-var task=generator.next(result);
+    var gen=generator.call(ctx);
+    var result;
+    while(true){
+    var task=generator.next(result);
 //...
 
-}
+    }
 
 }
