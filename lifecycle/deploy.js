f77f786732582c550236ea6ee5caf34aad780586
MikailBag <bagishov444.2002.02@gmail.com>
2015-11-20 20:10:21 +0300

    CI debug, isn't a stable

--- lifecycle/deploy.js
+++ lifecycle/deploy.js
@@ -1,4 +1,7 @@
-require('fs').createWriteStream('./example/sample.txt').write('just sample', function cb(err) {
+var fs=require('fs');
+fs.createWriteStream('./example/sample.txt').write('just sample', function cb(err) {
+console.log('sample.txt created successfully');
+ console.log('current content of ./example dir is ',fs.readdirSync('./example'));
 
     process.exit(err ? 1 : 0);
 });
