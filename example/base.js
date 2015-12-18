5f399323366ff43bcaf1d8c97fb115d01541032c
MikailBag <bagishov444.2002.02@gmail.com>
2015-12-18 19:58:17 +0300

    some es6 and bugfix

--- example/base.js
+++ example/base.js
@@ -32,7 +32,7 @@ function apiFn(name, signature, desc) {
 genio(function* (io) {
 //intro
     debugger;
-    //console.error(`io is ${util.inspect(io)}`);
+    var fd;
     console.log(['this is example of some features of gen-io',
         'install: npm i gen-io -S',
         'usage: genfs(function* (io){generator, using io () objects? containing async calls? supported by gen-io})']
@@ -50,7 +50,8 @@ genio(function* (io) {
     var ports = yield io.dns.resolve('google.com', 'A');
     console.log('Google is running on following ports:');
     cursor.yellow();
-    for (var port in ports) {
+
+     for (var port in ports) {
         cursor.write(`\t${ports[port]}\n`)
     }
     cursor.reset();
@@ -61,23 +62,23 @@ genio(function* (io) {
     console.log('lets now know fd of sample by event "open" and its first chunk');
     var stream = fs.createReadStream(sample);
     fd = yield io.util.wait(stream, 'open');
-    var filepart = yield io.util.read(stream);
+    var filepart  = yield io.util.read(stream);
     console.log('first chunk of sample is %s, its fd is %s',filepart,fd);
     part('plugin API');
     console.log('now it isn\'t very big (consists of 1 function), but it can be useful. Example:\n'+
     'new io component:foo foo()->\'bar\'');
     io = genio.ctx({
-        foojs: {
+           foojs: {
             foo: function foo(cb) {
                 process.nextTick(function () {
                         cb(null, 'bar')
-                    }
-                );
+                  }
+               );
             }
         }
     });
     var bar = yield io.foojs.foo();
     assert.equal('bar', bar);
     console.log('and bar is %s',bar)
-});
+ });
 //FIXME undefined in output
