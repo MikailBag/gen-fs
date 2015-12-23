1a76e1e25a25a86a65900c715c4a99477e3eb72e
MikailBag <bagishov444.2002.02@gmail.com>
2015-12-23 21:11:29 +0300

    now test is async (without gen-io)

--- example/redis.js
+++ example/redis.js
@@ -2,11 +2,22 @@
 const redis=require('redis');
 const genio=require('../lib');
 const client=redis.createClient();
-genio(function*(){
+/*genio(function*(){
     let val;
     let redis=new genio.Context(client);
     val='works with Redis';
     yield redis.set('gen-io',val);
     val=yield redis.get('gen-io');
     console.log(`gen-io ${val}`);
+});*/
+client.set('ggg','val',function(err){
+    if(err){
+        console.error(err);
+    }
+    client.get('ggg',function(err,val){
+        if(err){
+            console.error(err);
+        }
+        console.log(val)
+    })
 });
